c1bb2df2afc747351946413e3e3cbe9c92a3c348
brandon <brandon@dacrib.net>
2014-08-06 19:09:46 -0400

    add support for screenUp
    decoded but unverified

--- faviremote.ino
+++ faviremote.ino
@@ -9,14 +9,21 @@ void setup()
 
 void loop()
 {
+	//screen down
      screenDown();
-     delay(10);
+     delay(100);
      screenDown();
-     delay(10);
+     delay(100);
      screenDown();
-     delay(10000);
-     //screenUp();
-     //delay(10);
+     delay(1000000);
+
+     //screen up
+     screenUp();
+     delay(100);
+     screenUp();
+     delay(100);
+     screenUp();
+     delay(1000000);
 }
 
 void customDelay(unsigned long time) 
@@ -38,64 +45,114 @@ void setStateWithDelay(int pin, int state,int delayTime)
 
 void screenUp()
 {
+	Serial.print("transmitting ScreenUp\n");
 
+	setStateWithDelay(transmitPin,1,200);//1
+	setStateWithDelay(transmitPin,0,400);
+	setStateWithDelay(transmitPin,1,500);
+	setStateWithDelay(transmitPin,0,100);
+	setStateWithDelay(transmitPin,1,200);//5
+	setStateWithDelay(transmitPin,0,400);
+	setStateWithDelay(transmitPin,1,500);
+	setStateWithDelay(transmitPin,0,100);
+	setStateWithDelay(transmitPin,1,200);
+	setStateWithDelay(transmitPin,0,400);//10
+	setStateWithDelay(transmitPin,1,200);
+	setStateWithDelay(transmitPin,0,400);
+	setStateWithDelay(transmitPin,1,200);
+	setStateWithDelay(transmitPin,0,400);
+	setStateWithDelay(transmitPin,1,200);//15
+	setStateWithDelay(transmitPin,0,400);
+	setStateWithDelay(transmitPin,1,500);
+	setStateWithDelay(transmitPin,0,100);
+	setStateWithDelay(transmitPin,1,200);
+	setStateWithDelay(transmitPin,0,400);//20
+	setStateWithDelay(transmitPin,1,400);
+	setStateWithDelay(transmitPin,0,200);
+	setStateWithDelay(transmitPin,1,400);
+	setStateWithDelay(transmitPin,0,200);
+	setStateWithDelay(transmitPin,1,400);//25
+	setStateWithDelay(transmitPin,0,200);
+	setStateWithDelay(transmitPin,1,100);
+	setStateWithDelay(transmitPin,0,500);
+	setStateWithDelay(transmitPin,1,400);
+	setStateWithDelay(transmitPin,0,200);//30
+	setStateWithDelay(transmitPin,1,400);
+	setStateWithDelay(transmitPin,0,200);
+	setStateWithDelay(transmitPin,1,400);
+	setStateWithDelay(transmitPin,0,200);
+	setStateWithDelay(transmitPin,1,100);//35
+	setStateWithDelay(transmitPin,0,500);
+	setStateWithDelay(transmitPin,1,100);
+	setStateWithDelay(transmitPin,0,500);
+	setStateWithDelay(transmitPin,1,100);
+	setStateWithDelay(transmitPin,0,500);//40
+	setStateWithDelay(transmitPin,1,100);
+	setStateWithDelay(transmitPin,0,500);
+	setStateWithDelay(transmitPin,1,100);
+	setStateWithDelay(transmitPin,0,500);
+	setStateWithDelay(transmitPin,1,100);//45
+	setStateWithDelay(transmitPin,0,500);
+	setStateWithDelay(transmitPin,1,100);
 	digitalWrite(transmitPin,LOW);
+
+	Serial.print("transmitted ScreenUp\n");
 }
 
 void screenDown()
 {
 	Serial.print("transmitting ScreenDown\n");
 
-	setStateWithDelay(transmitPin,1,200);
+	setStateWithDelay(transmitPin,1,200);//1
+	setStateWithDelay(transmitPin,0,400);
+	setStateWithDelay(transmitPin,1,500);
+	setStateWithDelay(transmitPin,0,100);
+	setStateWithDelay(transmitPin,1,200);//5
 	setStateWithDelay(transmitPin,0,400);
 	setStateWithDelay(transmitPin,1,500);
 	setStateWithDelay(transmitPin,0,100);
 	setStateWithDelay(transmitPin,1,200);
-	setStateWithDelay(transmitPin,0,400);
-	setStateWithDelay(transmitPin,1,500);
-	setStateWithDelay(transmitPin,0,100);
+	setStateWithDelay(transmitPin,0,400);//10
 	setStateWithDelay(transmitPin,1,200);
 	setStateWithDelay(transmitPin,0,400);
 	setStateWithDelay(transmitPin,1,200);
 	setStateWithDelay(transmitPin,0,400);
-	setStateWithDelay(transmitPin,1,200);
-	setStateWithDelay(transmitPin,0,400);
-	setStateWithDelay(transmitPin,1,200);
+	setStateWithDelay(transmitPin,1,200);//15
 	setStateWithDelay(transmitPin,0,400);
 	setStateWithDelay(transmitPin,1,200);
 	setStateWithDelay(transmitPin,0,400);
 	setStateWithDelay(transmitPin,1,500);
-	setStateWithDelay(transmitPin,0,100);
+	setStateWithDelay(transmitPin,0,100);//20
 	setStateWithDelay(transmitPin,1,200);
 	setStateWithDelay(transmitPin,0,400);
 	setStateWithDelay(transmitPin,1,500);
 	setStateWithDelay(transmitPin,0,100);
-	setStateWithDelay(transmitPin,1,400);
+	setStateWithDelay(transmitPin,1,400);//25
 	setStateWithDelay(transmitPin,0,200);
 	setStateWithDelay(transmitPin,1,400);
 	setStateWithDelay(transmitPin,0,200);
 	setStateWithDelay(transmitPin,1,100);
-	setStateWithDelay(transmitPin,0,500);
+	setStateWithDelay(transmitPin,0,500);//30
 	setStateWithDelay(transmitPin,1,400);
 	setStateWithDelay(transmitPin,0,200);
 	setStateWithDelay(transmitPin,1,100);
 	setStateWithDelay(transmitPin,0,500);
-	setStateWithDelay(transmitPin,1,100);
+	setStateWithDelay(transmitPin,1,100);//35
 	setStateWithDelay(transmitPin,0,500);
 	setStateWithDelay(transmitPin,1,100);
 	setStateWithDelay(transmitPin,0,500);
 	setStateWithDelay(transmitPin,1,100);
-	setStateWithDelay(transmitPin,0,500);
+	setStateWithDelay(transmitPin,0,500);//40
 	setStateWithDelay(transmitPin,1,100);
 	setStateWithDelay(transmitPin,0,500);
 	setStateWithDelay(transmitPin,1,100);
 	setStateWithDelay(transmitPin,0,500);
-	setStateWithDelay(transmitPin,1,400);
+	setStateWithDelay(transmitPin,1,400);//45
 	setStateWithDelay(transmitPin,0,200);
 	setStateWithDelay(transmitPin,1,400);
 	setStateWithDelay(transmitPin,0,200);
 	setStateWithDelay(transmitPin,1,100);
-	setStateWithDelay(transmitPin,0,10000);
+	setStateWithDelay(transmitPin,0,10000);//50
 	digitalWrite(transmitPin,LOW);
     
     Serial.print("transmitted ScreenDown\n");
